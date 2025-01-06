f639610de6e8f59ca3445c0fdd2e94f52f52294e
ahmetucar1 <213214006@stu.adu.edu.tr>
2025-01-06 21:45:51 +0300

    Added first project

---
+++ Pratik1-MyfirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+ namespace  HelloWorld
+ {
+    class Program 
+    {
+        static void Main(String[]args)
+        {
+            Console.WriteLine("Ahmet Uçar.");
+            Console.WriteLine("Patika Bootcampinde olmamın sebebi yazılımcı olup, kariyerime ilk adımı güvenle atmak.")
+        }
+    }
+ }
