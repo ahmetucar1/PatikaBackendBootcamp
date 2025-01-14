7f8001f98efdff8c7d86e5829d0f2ed831c85079
ahmetucar1 <213214006@stu.adu.edu.tr>
2025-01-14 21:26:24 +0300

    added new project

---
+++ Week-5/Pratice-20-ListCoffe/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace coffee_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Coffee = new List<string>();
+            string coffes;
+
+            Console.WriteLine("5 Adet kahve giriniz.");
+            for (int i = 0; i < 5; i++)
+            {
+                coffes = Console.ReadLine();
+                Coffee.Add(coffes);
+            }
+            Console.WriteLine("**Kahveleriniz**");
+            for (int i = 0;i < 5; i++)
+            {
+                Console.WriteLine(i+1+"-"+Coffee[i]);
+            }
+        }
+    }
+}
