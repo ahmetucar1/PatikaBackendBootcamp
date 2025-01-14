566c0954e90a808d3f7374a1d0a108a145678714
ahmetucar1 <213214006@stu.adu.edu.tr>
2025-01-14 18:23:19 +0300

    new project added

---
+++ Week-5/Pratice19-Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Yeni Liste tanımlama
+            List<string> davetliler = new List<string>();
+
+            //Listeye eleman ekleme
+            davetliler.Add("Bülent Ersoy");
+            davetliler.Add("Ajda Pekkan");
+            davetliler.Add("Ebru Gündeş");
+            davetliler.Add("Hadise");
+            davetliler.Add("Hande Yener");
+            davetliler.Add("Tarkan");
+            davetliler.Add("Funda Arar");
+            davetliler.Add("Demet Akalın");
+
+            //Listeyi yazdırma
+            foreach (var davetli in davetliler)
+            {
+                Console.WriteLine(davetli);
+            }
+
+            //Ek olarak: Kullanıcıdan bir davetli ismi alıp
+            Console.WriteLine("Sen de bir tane ekle");
+            string yeniDavetli = Console.ReadLine();
+
+            //alınan ismi listeye ekleme
+            davetliler.Add(yeniDavetli);
+
+            Console.WriteLine("Yeni Davetli Listesi");
+
+            //Yeni Listeyi yazdırma
+            foreach (var davetli in davetliler)
+            {
+                Console.WriteLine(davetli);
+            }
+
+        }
+    }
+}
